20a7f802679145e6af12cbb17154799dd1284b6b
Vladyslav <vladyslav36@gmail.com>
2022-05-01 12:43:51 +0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express=require('express')
-const cors=require('cors')
-const fileUpload = require('express-fileupload') 
-const slugify = require('slugify')
+const express = require("express")
+const cors = require("cors")
+const fileUpload = require("express-fileupload")
+const slugify = require("slugify")
 const uniqueSlug = require("unique-slug")
-const path=require('path')
+const path = require("path")
 const app = express()
 
 app.use(cors())
@@ -11,27 +11,28 @@ app.use(express.json())
 app.use(fileUpload())
 const PORT = 5001
 
-app.post('/upload', (req, res) => {
-
+app.post("/upload", (req, res) => {
   if (req.files === null) {
-   return res.status(400).json({msg:'Not file uploaded'})
+    return res.status(400).json({ msg: "Not file uploaded" })
   }
   console.log(req.body)
-  const  files = Array.isArray(req.files.files)?req.files.files:[req.files.files]
+  const files = Array.isArray(req.files.files)
+    ? req.files.files
+    : [req.files.files]
 
-  files.forEach(file => {
+  files.forEach((file) => {
     const name = path.parse(file.name).name
     const ext = path.parse(file.name).ext
     const slug =
-      slugify(name, { remove: /[*+~.()'"!:@ ]/g }) + '-' + uniqueSlug(name) + ext
+      slugify(name, { remove: /[*+~.()'"!:@ ]/g }) +
+      "-" +
+      uniqueSlug(name) +
+      ext
     console.log(slug)
     file.mv(`${__dirname}/client/public/upload/${slug}`)
-    
   })
 
   res.status(200).json({})
-  
 })
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
-
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
